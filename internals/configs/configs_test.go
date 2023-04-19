7fe1b4ad14adb23b8b625d34f3f8e74ded5ef3bb
sina p <sp7192@gmail.com>
2023-04-20 01:11:30 +0430

    Adding config test with env

--- internals/configs/configs_test.go
+++ internals/configs/configs_test.go
@@ -1,6 +1,7 @@
 package configs
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,5 +12,14 @@ func TestLoadConfigs(t *testing.T) {
 	assert.NoError(t, err)
 
 	var expected uint = 4
+	assert.Equal(t, conf.PlayersInRoomCount, expected)
+}
+
+func TestLoadConfigsWithEnv(t *testing.T) {
+	os.Setenv("PLAYERS_IN_ROOM_COUNT", "5")
+	conf, err := LoadConfigs("./testdata/", "testconfigs", "env")
+	assert.NoError(t, err)
+
+	var expected uint = 5
 	assert.Equal(t, conf.PlayersInRoomCount, expected)
 }
